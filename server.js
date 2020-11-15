b368e3cb8299a435d00e1d164cdc5e530cc37664
Joaquín <joaco1509@gmail.com>
2020-11-14 22:44:03 -0300

    commit

--- server.js
+++ server.js
@@ -27,19 +27,17 @@ app.use(express.json());
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
-
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    //res.header('Access-Control-Allow-Origin', "http://127.0.0.1:3000");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Set-Cookie');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
     if ('OPTIONS' == req.method) {
          res.send(200);
      } else {
          next();
      }
-});
+    });
 
 //Passport
 
