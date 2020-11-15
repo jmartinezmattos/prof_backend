b78ddc7fa9c1f6b174592d4da717a135d29c8e9e
Joaquín <joaco1509@gmail.com>
2020-11-14 23:42:04 -0300

    commit

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     //res.header('Access-Control-Allow-Origin', "http//localhost:3000");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization, Cookie');
     if ('OPTIONS' == req.method) {
          res.send(200);
      } else {
