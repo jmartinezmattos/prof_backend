706c1092f5f5a7b6596e2dc5ff7bbbbe2d326e34
Martin Brian <martin.brian1998@gmail.com>
2020-11-17 02:11:13 -0300

    prueba con localhost

--- server.js
+++ server.js
@@ -38,8 +38,9 @@ app.all('/', function(req, res, next) {
 
 app.use(function(req, res, next) {
     // res.header('Access-Control-Allow-Credentials', true);
-    // res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    //res.header('Access-Control-Allow-Origin', "http//localhost:3000");
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    //res.header('Access-Control-Allow-Origin', "http//localhost:3000")
+    res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Authorization, Cookie');
     if ('OPTIONS' == req.method) {
